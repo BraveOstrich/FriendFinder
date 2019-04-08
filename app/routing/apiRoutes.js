aa5c257a64093770974169182914c7b63b1e753e
Steve White <stevewhitecodes@gmail.com>
2019-04-08 16:53:45 -0500

    Updating to save changes

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,11 @@
+// === Variables === //
+
+var friendsData = require("../data/friends");
+
+module.exports = function(app){
+    app.get("/api/friends", function(req, res){
+        res.json(friends)
+    });
+
+    
+}
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,7 +1,20 @@
-app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "survey.html"));
+// === Variables === //
+var path = require("path");
+
+
+
+
+// === Routing === //
+
+module.exports = function(app){
+
+
+  app.get("/survey", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
-  });
+};
